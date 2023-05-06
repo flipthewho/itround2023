9c3b72c43aecae0449b183890bc2fa9e30ebcc80
flipthewho <ya411508@gmail.com>
2023-05-06 19:04:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+
+# Устанавливаем токен и создаем экземпляр бота
+TOKEN = 'xxx'
+bot = telegram.Bot(token=TOKEN)
 
 
+# Функция-обработчик команды /start
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот.")
+    context.bot.send_message(chat_id=update.effective_chat.id,\
+                             text="Приветствую! Я - бот ServerGuider, который поможет вам рассчитать стоимость затрат на обслуживание серверного  оборудования!")
+
+
+# Функция-обработчик текстовых сообщений
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+# Создаем объект Updater и привязываем функции-обработчики к командам и сообщениям
+updater = Updater(bot.token)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+#dispatcher.add_handler(MessageHandler(filters.text, echo))
+
+# Запускаем бота
+updater.start_polling()
+updater.idle()
